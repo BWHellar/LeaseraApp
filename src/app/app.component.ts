655ae594841a44eae6bb34e4b5cf53775dac94f7
Brendan Hellar <bwhellar@gmail.com>
2019-08-10 16:31:31 -0700

    cutting out unneeded code and adding styling choices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { auth } from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'LeaseraApp';
+export class AppComponent implements OnInit {
+  items: any[];
+  email: string;
+  password: string;
+
+  constructor(private Auth: AngularFireAuth, data: AngularFireDatabase){
+    data.list('/items')
+      .valueChanges()
+      .subscribe(items => {
+        this.items = items;
+        console.log(items)
+      });
+  }
+
+
+  ngOnInit() {
+    this.Auth.authState
+      .subscribe((user) => console.log(user));
+  }
+
+  signIn(){
+    this.Auth.auth.signInWithEmailAndPassword(this.email, this.password)
+      .then((userCred) => console.log(userCred));
+  }
+  logout() {
+    this.Auth.auth.signOut();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FirebaseModule } from './firebase/firebase.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFirestore } from 'angularfire2/firestore'
+import { AngularFireModule } from 'angularfire2';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
     BrowserModule,
-    FirebaseModule
+    FormsModule,
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [ AngularFirestore],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
